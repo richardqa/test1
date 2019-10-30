a26de4826a9481e8c61385ee8a89a9018ecc2af3
usr_linux <usr_linux@kiyu.minsa.gob.pe>
2019-10-30 12:13:17 -0500

    update

--- src/main/java/pe/gob/minsa/mcs/servicio/renhice/service/RegistraDatosClinicosService.java
+++
@@ -1,9 +0,0 @@
-package pe.gob.minsa.mcs.servicio.renhice.service;
-
-import pe.gob.minsa.mcs.servicio.renhice.bean.DatosClinicosBean;
-
-public interface RegistraDatosClinicosService {
-	
-	String registraDatosClinicos(DatosClinicosBean datosClinicosBean);
-
-}
